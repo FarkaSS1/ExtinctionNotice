18cd62f782fe7568d921766c011426687608698d
mikaelmotin <motin@chalmers.se>
2025-02-24 17:43:45 +0100

    remove: heal with 'D'

--- Assets/Scripts/Healthsystem/PlayerHealth.cs
+++ Assets/Scripts/Healthsystem/PlayerHealth.cs
@@ -4,10 +4,10 @@ public class PlayerHealth : Health
 {
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))  // Press "D" to take damage
-        {
-            TakeDamage(10);  // Damage the player by 10
-        }
+        // if (Input.GetKeyDown(KeyCode.D))  // Press "D" to take damage (for testing purposes)
+        // {
+        //     TakeDamage(10);  // Damage the player by 10
+        // }
 
         if (Input.GetKeyDown(KeyCode.H))  // Press "H" to heal
         {
