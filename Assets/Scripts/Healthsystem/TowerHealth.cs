0d810d20339e3b8d0a2a124bff3ac1859c88a031
DavidGidlof <117651607+Ladibo@users.noreply.github.com>
2025-02-27 11:45:40 +0100

    meh

--- Assets/Scripts/Healthsystem/TowerHealth.cs
+++ Assets/Scripts/Healthsystem/TowerHealth.cs
@@ -1,53 +1,14 @@
 using UnityEngine;
+using System;
 
-public class TowerHealth : MonoBehaviour, IHealth
+public class TowerHealth : Health
 {
-    [SerializeField] private float maxHealth = 1000f;
-    private float currentHealth;
+    public event Action OnDestroyed; // Notify when destroyed
 
-    public event System.Action<float> OnHealthChanged; // Update UI
-    public event System.Action OnDestroyed; // Notify when destroyed
-
-    private void Awake()
+    protected override void Die()
     {
-        currentHealth = maxHealth;
-    }
-
-    public void TakeDamage(float damage)
-    {
-        currentHealth -= damage;
-        OnHealthChanged?.Invoke(currentHealth / maxHealth); // Send UI update
-
-        Debug.Log(gameObject.name + " took " + damage + " damage. Remaining health: " + currentHealth);
-
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
-    public void TakeDamage(float damage, Transform attacker)
-    {
-        currentHealth -= damage;
-        OnHealthChanged?.Invoke(currentHealth / maxHealth);
-
-        Debug.Log(gameObject.name + " took " + damage + " damage from " + (attacker != null ? attacker.name : "Unknown"));
-
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
-
-    public void Heal(float amount)
-    {
-        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
-        OnHealthChanged?.Invoke(currentHealth / maxHealth);
-    }
-
-    private void Die()
-    {
-        Debug.Log(gameObject.name + " has been destroyed!");
+        Debug.Log($"{gameObject.name} has been destroyed!");
         OnDestroyed?.Invoke();
-        Destroy(gameObject);
+        base.Die(); // Calls the base Die() method to handle destruction
     }
 }
